eee4d035a18be102df3dfdf0fb82e6efa1cd5032
ZadokhinDima <dimazadohin@gmail.com>
2017-08-18 13:42:50 +0300

    deleting dublicated constants

--- src/Constants.java
+++
@@ -1,6 +0,0 @@
-
-public interface Constants {
-    String DB_URL = "jdbc:mysql://localhost:3306/kredits?verifyServerCertificate=false&useSSL=true";
-    String DB_USER = "root";
-    String DB_PASSWORD = "dimazadokhin";
-}
